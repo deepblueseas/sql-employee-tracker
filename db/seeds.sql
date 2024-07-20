9d7ef46b751ae51408f5700230f4114f3735bc3c
Tess McGovern <ren.mcgovern@gmail.com>
2024-07-19 20:41:14 -0400

    moved schema into db and started setting up the seeds

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO employees_db (first_name, last_name, role_id, manager_id)
+VALUES
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+
+INSERT INTO department (department_name)
+VALUES
